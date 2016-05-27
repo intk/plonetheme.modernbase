2a24b91099e65e611a9f142d7e7d9afeac5c0e4a
andreesg <andreslb1@gmail.com>
2016-05-27 09:26:46 +0200

    Add Top menu fullscreen navigation solution

--- src/plonetheme/modernbase/theme/js/bundle.js
+++ src/plonetheme/modernbase/theme/js/bundle.js
@@ -9,6 +9,29 @@ if (window.jQuery) {
   });
 }
 
+
+
+$(document).ready(function($){
+  var isLateralNavAnimating = false;
+  
+  //open/close lateral navigation
+  $('.cd-nav-trigger').on('click', function(event){
+    event.preventDefault();
+    //stop if nav animation is running 
+    if( !isLateralNavAnimating ) {
+      if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+      
+      $('body').toggleClass('navigation-is-open');
+      $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+        //animation is over
+        isLateralNavAnimating = false;
+      });
+    }
+  });
+});
+
+
+
 require([
   'jquery',
 ], function($, dep1, logger){
@@ -20,7 +43,6 @@ require([
       $('body').addClass('modernbase-main');
     });
   }
-
 });
 
 
